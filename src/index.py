36b7e9b53d5349288c17d47701aa3fde63a2724e
bipblop555 <tom.bonnet@hotmail.com>
2023-11-22 16:24:26 +0100

    :( farid

--- src/index.py
+++ src/index.py
@@ -7,17 +7,14 @@ import os
 app = Flask(__name__)
 
 def convert_image_to_pdf(image):
-    # Sauvegarder temporairement l'image sur le disque
     temp_filename = 'temp_image.png'
     Image.open(image).save(temp_filename)
 
-    # Convertir l'image en PDF
     pdf_data = BytesIO()
     pdf = canvas.Canvas(pdf_data)
     pdf.drawImage(temp_filename, 10, 10, width=180, height=180)
     pdf.save()
 
-    # Supprimer le fichier temporaire
     os.remove(temp_filename)
 
     return pdf_data.getvalue()
@@ -38,15 +35,13 @@ def upload():
     if image.filename == '':
         return redirect(url_for('index'))
 
-    # Vérifier si le fichier est une image
     if image and image.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-        # Convertir l'image en PDF
         pdf_data = convert_image_to_pdf(image)
 
-        # Retourner le PDF généré
         return send_file(BytesIO(pdf_data), download_name='output.pdf', as_attachment=True, mimetype='application/pdf')
     else:
         return "Le fichier téléchargé n'est pas une image valide."
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
